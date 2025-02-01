94b7c1e86857eefc6c44c89b46da779228a1d6eb
Andrew Halisky <ajhalisky@gmail.com>
2025-02-01 00:45:39 -0500

    Updated Page
    Uncommented Page function

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
 
-// export default function Page() {
-//     return <p>Customers Page</p>;
-//   }
+export default function Page() {
+    return <p>Customers Page</p>;
+  }
 
   export const metadata: Metadata = {
     title: 'Customers Page',
