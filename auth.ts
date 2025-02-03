de69f272329ced0fe22cb6cdd42f7892037e7dc6
Andrew Halisky <ajhalisky@gmail.com>
2025-02-02 19:56:55 -0500

    Fixed app error
    Fixed app error from code

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email} LIMIT 1`;
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
@@ -16,24 +16,24 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-const authOptions = {
-  ...authConfig,
-  providers: [Credentials({
-    async authorize(credentials) {
-      const parsedCredentials = z
-        .object({ email: z.string().email(), password: z.string().min(6) })
-        .safeParse(credentials);
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [Credentials({
+        async authorize(credentials) {
+            const parsedCredentials = z
+                .object({ email: z.string().email(), password: z.string().min(6) })
+                .safeParse(credentials);
 
-      if (!parsedCredentials.success) return null
-      const { email, password } = parsedCredentials.data;
-      const user = await getUser(email);
-      if (!user) return null;
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
 
-      const passwordsMatch = await bcrypt.compare(password, user.password);
-      return passwordsMatch ? user : null;
-    },
-  }),
-  ],
-};
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+        return null;
+        },
+    }),
+    ],
+});
